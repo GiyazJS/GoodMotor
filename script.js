9d74239ce4a3efc1a6d1d3766242fe2bec932258
Giyaz <atazhanovgiyaz@gmail.com>
2021-07-16 15:59:57 +0600

    slider

---
+++ script.js
@@ -0,0 +1,11 @@
+$('.section__abouts').slick ({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+})
+
+$('.section__videos').slick ({
+    slidesToShow: 3,
+    slidesToScroll: 1
+})
+    
